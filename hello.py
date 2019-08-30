ee2e2cb82e9e6eaf51396fcf8a441fe689cf5d0b
Sebastian Ballarati <sebastianballarati@gmail.com>
2019-08-30 11:35:21 -0400

    working with oauth2 and a github client

--- hello.py
+++ hello.py
@@ -1,7 +1,42 @@
 from flask import Flask
+from flask import redirect, url_for
+#, session, request, jsonify, render_template
+from authlib.flask.client import OAuth
+# for later, try from auth0.v3.authentication import Social
 
 app = Flask(__name__)
+app.secret_key = 'a secret key'
+oauth = OAuth(app)
+
+#OAuth2 login
+oauth.register(
+    name='github',
+    client_id='YOURGITHUBCLIENTID',
+    client_secret='YOURGITHUBSECRET',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'user:email'},
+)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, OAuth World!"
+
+@app.route("/worked")
+def worked():
+    return "Authenticated with GitHub :)"
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorize', _external=True)
+    return oauth.github.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.github.authorize_access_token()
+    resp = oauth.github.get('user')
+    profile = resp.json()
+    print(profile)
+    # do something with the token and profile
+    return redirect('/worked')
